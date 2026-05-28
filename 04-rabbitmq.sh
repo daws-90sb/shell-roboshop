92c35893a6a1d7eee3153b4f07b54f95a7d1f67a
joindevops90sb <lokam.nithisha@gmail.com>
2026-05-28 10:48:20 +0530

    commit file

--- 04-rabbitmq.sh
+++ 04-rabbitmq.sh
@@ -32,7 +32,7 @@ VALIDATE() {
 
 }
 
-cp rabbitmq.repo /etc/yum repos.d/rabbitmq.repo
+cp rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
 VALIDATE $? "Adding rabbitmq repo"
 
 dnf install rabbitmq-server -y
