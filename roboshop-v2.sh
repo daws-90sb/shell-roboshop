8a5be96c756208f58c36b39f141bf02d5a7e3e19
joindevops90sb <lokam.nithisha@gmail.com>
2026-05-28 23:46:02 +0530

    commit file

--- roboshop-v2.sh
+++ roboshop-v2.sh
@@ -13,7 +13,7 @@ N="\e[0m"
 
 ### validation error ###
 if [ $# -lt 2 ]; then
-     echo -2 "$R ERROR :: Alteast 2 arguments required $N"
+     echo -e "$R ERROR :: Alteast 2 arguments required $N"
      echo "USAGE: $0 [create/delete] [instance1] [instance2...]"
      exit 1
 fi    
@@ -23,7 +23,7 @@ ACTION=$1
 shift # first argument will be removed, so $@ does not have action/destroy
 
 
-destroy
+
 
 if [ "$ACTION" != "create" ]  && [ "$ACTION" != "delete"]; then
     echo -e " $R ERROR:: First argument must be either create/delete $N"
