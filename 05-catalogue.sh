928e10589b2f7d99369819532367c8dbe2b73ae1
joindevops90sb <lokam.nithisha@gmail.com>
2026-05-28 13:08:09 +0530

    commit file

--- 05-catalogue.sh
+++ 05-catalogue.sh
@@ -7,6 +7,7 @@ sudo mkdir -p $LOGS_FOLDER
 sudo chown -R ec2-user:ec2-user $LOGS_FOLDER
 sudo chmod -R 755 $LOGS_FOLDER
 LOGS_FILE="$LOGS_FOLDER/$0.log"
+SCRIPT_DIR=$PWD
 
 USERID=$(id -u)
 R="\e[31m"
@@ -44,9 +45,30 @@ else
     echo -e "sytem user roboshop already created .....$Y SKIPPING $N"
 fi        
 
+rm -rf /app
+VALIDATE $? "removing existing code"
+
+rm -rf /tmp/catalogue.zip
+VALIDATE $? "removed catalogue zip"
+
 mkdir -p /app  &>> $LOGS_FILE
 VALIDATE $? "Creating App Directory"
 
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip 
 cd /app 
 unzip /tmp/catalogue.zip
+VALIDATE $? "Downloaded and extracted catalogue code"
+
+npm install 
+VALIDATE $? "Installing Dependencies"
+
+cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
+VALIDATE $? "Created systemctl service"
+
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Added Mongo Repo"
+
+dnf install mongodb-mongosh -y
+VALIDATE $? "Installed Mongodb Client"
+
+
