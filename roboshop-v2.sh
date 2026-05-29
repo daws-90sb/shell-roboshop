628541c02cbdeb23af5226d9ca9d5fb1de9f7fd9
daws-90sb <lokam.nithisha@gmail.com>
2026-05-30 00:03:36 +0530

    changes added

--- roboshop-v2.sh
+++ roboshop-v2.sh
@@ -47,7 +47,7 @@ do
                --image-id $AMI_ID \
                --instance-type t3.micro \
                --security-groups "roboshop-common" "roboshop-$instance" \
-               --tag-specifications "ResourceType=instance,Tags=[{Key=Name Value="roboshop-$instance"}]" \
+               --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value="roboshop-$instance"}]" \
                --query 'Instances[0].InstanceId' \
                --output text
                )
