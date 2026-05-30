dbe3223d7fff2dc34b916230a7efe1003a5562e7
daws-90sb <lokam.nithisha@gmail.com>
2026-05-31 02:22:59 +0530

    changes added

---
+++ 09-payment.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+
+
+LOGS_FOLDER="/var/log/roboshop"
+sudo mkdir -p $LOGS_FOLDER
+sudo chown -R ec2-user:ec2-user $LOGS_FOLDER
+sudo chmod -R 755 $LOGS_FOLDER
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+SCRIPT_DIR=$PWD
+MYSQL_HOST=mysql.daws-90sb.online
+
+CARTID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+TIMESTAMP=$(date "+%y-%m-%d %H:%M:%S")
+
+
+if [ $CARTID -ne 0 ]; then 
+    echo -e " $TIMESTAMP [ERROR] $R please run this script with root access $N" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+VALIDATE() { 
+    if [ $1 -ne 0 ]; then
+          echo -e " $TIMESTAMP [ERROR] $2.....$R FAILURE $N " | tee -a $LOGS_FILE
+          exit 1
+    else
+          echo -e " $TIMESTAMP [INFO] $2.....$G SUCCESS $N " | tee -a $LOGS_FILE
+    fi             
+
+}
+
+
+dnf install python3 gcc python3-devel -y  &>> $LOGS_FILE
+VALIDATE $?  "Installing Python"
+
+id roboshop &>> $LOGS_FILE
+if [ $? -ne 0 ]; then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>> $LOGS_FILE 
+    VALIDATE $? "Creating roboshop systemuser"
+else 
+    echo -e "sytem user roboshop already created .....$Y SKIPPING $N"
+fi        
+
+rm -rf /app
+VALIDATE $? "removing existing code"
+
+rm -rf /tmp/payment.zip
+VALIDATE $? "removed payment zip"
+
+mkdir -p /app  &>> $LOGS_FILE
+VALIDATE $? "Creating App Directory"
+
+curl -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip &>> $LOGS_FILE
+cd /app 
+unzip /tmp/payment.zip &>> $LOGS_FILE
+VALIDATE $? "Downloaded and extracted payment code"
+
+cd /app 
+pip3 install -r requirements.txt  &>> $LOGS_FILE
+VALIDATE $? "Installing Dependencies"
+
+cp $SCRIPT_DIR/shipping.service /etc/systemd/system/payment.service
+VALIDATE $? "Created systemctl service"
+
+
+   systemctl enable payment 
+   systemctl restart payment
+   VALIDATE $? "Enabling and Starting payment"
