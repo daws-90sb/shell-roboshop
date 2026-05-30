80018ab61308c695ed3ed3e0bba37bbc42bdd527
daws-90sb <lokam.nithisha@gmail.com>
2026-05-31 03:08:00 +0530

    changes added

--- 10-frontend.sh
+++ 10-frontend.sh
@@ -50,7 +50,7 @@ VALIDATE $? "Downloaded and extracted frontend code"
 rm -rf /etc/nginx/nginx.conf
 VALIDATE $? "Removed Default conf"
 
-cp nginx.conf /etc/nginx/nginx.conf 
+cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf 
 VALIDATE $? "copied roboshop nginx conf"
 
 systemctl restart nginx 
